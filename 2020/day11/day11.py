c6200c74e8ad9da41479fef6d1007f4cd2f18fc0
Cece78 <45494688+Cece78@users.noreply.github.com>
2020-12-14 21:03:35 +0000

    Full day 11

--- 2020/day11/day11.py
+++ 2020/day11/day11.py
@@ -10,16 +10,9 @@ import numpy as np
 with open('input_day11.txt', 'r') as file:
     puzzle = [[x for x in line.strip()] for line in file]
     puzzle = np.array(puzzle)
-    
-with open('example.txt', 'r') as file:
-    example = [[x.strip() for x in line] for line in file]
-    example = np.array(example)
-    
-with open('no_movement.txt', 'r') as file:
-    no_movement = [[x.strip() for x in line] for line in file]
-    no_movement = np.array(no_movement)
-    
-print(puzzle[:100])
+
+
+# Part 1
 
 def count_seats_taken(data):
     count = 0
@@ -28,9 +21,6 @@ def count_seats_taken(data):
             if seat == '#':
                 count+=1
     return count
-
-assert count_seats_taken(example) == 23
-assert count_seats_taken(puzzle) == 0
 
 
 def seat_to_free(x, y, data):
@@ -46,9 +36,6 @@ def seat_to_free(x, y, data):
             return True
     return False
 
-assert seat_to_free(3, 1, example) == True
-assert seat_to_free(5, 6, example) == False
-
 
 def seat_to_take(x, y, data):
     if data[x][y] != 'L':
@@ -59,9 +46,6 @@ def seat_to_take(x, y, data):
                 if data[i][j] == '#':
                     return False
     return True
-
-assert seat_to_take(2, 7, example) == True
-assert seat_to_take(3, 3, example) == False
 
 
 def get_seat_coordinates(data):
@@ -96,5 +80,67 @@ def run(data):
 
 print(run(puzzle))
     
-        
+
+# Part 2
+
+def count_visible_seats(x, y, data):
+    count = 0
+    directions = [(1,0), (-1,0), (0,1), (0, -1), (1,1), (-1,-1), (1,-1), (-1,1)]
+    for direction in directions:
+        i, j = x, y
+        while True: 
+            i, j = i+direction[0], j+direction[1]
+            if (0 <= i < len(data)) and (0 <= j < len(data[0])):
+                if data[i][j] == '#':
+                    count += 1
+                    break
+                elif data[i][j] == 'L':
+                    break
+            else:
+                break
+    return count
+
+
+def seat_to_free(x, y, data):
+    if data[x][y] != '#':
+        return False
+    else:
+        if count_visible_seats(x, y, data) >= 5:
+            return True
+    return False
+
+
+def seat_to_take(x, y, data):
+    if data[x][y] != 'L':
+        return False
+    else:
+       if count_visible_seats(x, y, data) ==0:
+           return True
+    return False
+
+
+def get_seat_coordinates(data):
+    to_free = set()
+    to_take = set()
+    for i, row in enumerate(data):
+        for j, col in enumerate(row):
+            if seat_to_free(i, j, data):
+                to_free.add((i,j))
+            elif seat_to_take(i, j, data):
+                to_take.add((i,j))         
+    return to_free, to_take
+
+
+def run(data):
+    changes = True
+    while changes:
+        to_free, to_take = get_seat_coordinates(data)
+        data = update_seats(data, to_free, to_take)
+        if (len(to_free) == 0 and len(to_take) == 0):
+            changes = False        
+    return count_seats_taken(data)
+
+
+print(run(puzzle))
+
     
