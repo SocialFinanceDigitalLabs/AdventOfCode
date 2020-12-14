26b5b5816861f84042bf1099b4855c24a34cd1e8
Cece78 <45494688+Cece78@users.noreply.github.com>
2020-12-14 19:27:03 +0000

    Day14 part 2

--- 2020/day14/day14.py
+++ 2020/day14/day14.py
@@ -13,21 +13,21 @@ with open('input_day14.txt', 'r') as file:
 
 # Part 1
 
-def parse(mask):
+def parse(mask, exclude):
     mask = mask.split(' = ')[1]
-    mask = {i:char for i, char in enumerate(mask) if char != 'X'}
+    mask = {i:char for i, char in enumerate(mask) if char != exclude}
     return mask
 
 
 def extract(memory):
     regex = r"mem\[(\d+)\] = (\d+)"
-    index = re.search(regex, memory).group(1)
+    index = int(re.search(regex, memory).group(1))
     number = int(re.search(regex, memory).group(2))
-    return index, f'{number:036b}'
+    return index, number
 
 
 def apply_mask(mask, number):
-    number = list(number)
+    number = list(f'{number:036b}')
     for k,v in mask.items():
         number[k]=v
     return "".join(number)
@@ -37,7 +37,7 @@ def update_memory(instructions):
     memory = {}
     for line in instructions:
         if 'mask' in line:
-            mask = parse(line)
+            mask = parse(line, 'X')
         else:
             index, number = extract(line)
             masked_number = apply_mask(mask, number)
@@ -49,5 +49,42 @@ def run(instructions):
     memory = update_memory(instructions)
     return sum([v for k, v in memory.items()])
 
+
+print(run(puzzle))
+
+
+# Part 2
+
+def apply_mask(mask, index):
+    index = list(f'{index:036b}')
+    for k,v in mask.items():
+        index[k]=v
+    return "".join(index)
+
+
+def generate(index):
+    # Base case
+    if 'X' not in index:
+        return [index]
+    # Other cases
+    x = index.find('X')
+    string1 = index[:x] + '0' + index[x+1:]
+    string2 = index[:x] + '1' + index[x+1:]
+    branch1 = generate(string1)
+    branch2 = generate(string2)
+    return branch1 + branch2
+
+
+def update_memory(instructions):
+    memory = {}
+    for line in instructions:
+        if 'mask' in line:
+            mask = parse(line, '0')
+        else:
+            index, number = extract(line)
+            indexes = generate(apply_mask(mask, index))
+            for i in indexes:
+                memory[int(i, 2)] = number
+    return memory
 
 print(run(puzzle))
