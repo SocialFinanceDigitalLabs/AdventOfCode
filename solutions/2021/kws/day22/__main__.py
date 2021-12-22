bf520d13def1a0dd5df2dbd244944016188aa5a6
Kaj Siebert <kaj@k-si.com>
2021-12-22 16:34:40 +0100

    Part 1

--- solutions/2021/kws/day22/__main__.py
+++ solutions/2021/kws/day22/__main__.py
@@ -1,30 +1,14 @@
 # Advent of Code 2021 Day 22
-from common import standard_setup
 import day22
-from day22 import database
+from common import standard_setup
 
 
 def main(*argv):
     lines = standard_setup(*argv)
 
-    with database.Database.open() as db:
-        for line in lines:
-            xr, yr, zr, value = day22.parse_input(line)
-            xr = day22.limit(*xr, -50, 50)
-            yr = day22.limit(*yr, -50, 50)
-            zr = day22.limit(*zr, -50, 50)
-            if xr is None or yr is None or zr is None:
-                continue
-            db.set_many(xr, yr, zr, value)
-        print(db.count_values(1))
+    boxes = [day22.StateBox.from_input(day22.parse_input(line)) for line in lines]
 
-    print("   ")
-
-    with database.Database.open() as db:
-        for line in lines:
-            xr, yr, zr, value = day22.parse_input(line)
-            db.set_many(xr, yr, zr, value)
-        print(db.count_values(1))
+    print(boxes)
 
 
 if __name__ == "__main__":
