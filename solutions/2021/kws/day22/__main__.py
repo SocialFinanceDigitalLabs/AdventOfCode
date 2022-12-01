94705ed9131cb730f7a7849c139e5d07bff95696
Kaj Siebert <kaj@k-si.com>
2022-12-01 07:36:16 +0100

    Some uncommitted additions from last year...

--- solutions/2021/kws/day22/__main__.py
+++ solutions/2021/kws/day22/__main__.py
@@ -6,9 +6,15 @@ from common import standard_setup
 def main(*argv):
     lines = standard_setup(*argv)
 
-    boxes = [day22.StateBox.from_input(day22.parse_input(line)) for line in lines]
+    boxes = [day22.StateBox.from_input(*day22.parse_input(line)) for line in lines]
 
-    print(boxes)
+    for b1 in boxes:
+        intersects = 0
+        for b2 in boxes:
+            if b1 is not b2 and b1.intersects(b2):
+                intersects += 1
+        if intersects == 0:
+            print(f"{b1} does not intersect with any other box")
 
 
 if __name__ == "__main__":
