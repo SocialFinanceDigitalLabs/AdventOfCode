28a518cd30f833a3e59ea82513c1f765868835c7
Kaj Siebert <kaj@k-si.com>
2022-12-01 08:31:07 +0100

    KWS DAY 01

---
+++ solutions/2022/kws/aoc_2022_kws/cli.py
@@ -0,0 +1,25 @@
+import importlib
+import os
+from pathlib import Path
+
+import click
+
+from .config import settings
+
+
+@click.group()
+@click.option("--user", "-u", type=str)
+def main(user):
+    if user:
+        settings.username = user
+        settings.save_settings()
+
+    if not settings.username:
+        user = click.prompt("Username", type=str, default=os.getlogin())
+        settings.username = user
+        settings.save_settings()
+
+
+for name in Path(__file__).parent.glob("day*.py"):
+    module = name.stem
+    mod = importlib.import_module(f"aoc_2022_kws.{module}")
