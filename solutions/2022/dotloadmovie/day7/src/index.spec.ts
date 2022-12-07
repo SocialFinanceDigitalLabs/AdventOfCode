71eaaceca7663a5c22d500aa0cae3118e93deb2b
Dave Tickle <dave.tickle@socialfinance.org.uk>
2022-12-07 12:19:34 +0000

    solution: dave day 7

---
+++ solutions/2022/dotloadmovie/day7/src/index.spec.ts
@@ -0,0 +1,14 @@
+import { solution1, solution2 } from "./index";
+
+describe("tests", () => {
+  test("test solution1", () => {
+    const output = solution1();
+    console.log(output);
+    expect(output).toEqual(1325919);
+  });
+  test("test solution2", () => {
+    const output = solution2();
+    console.log(output);
+    expect(output).toEqual(2050735);
+  });
+});
