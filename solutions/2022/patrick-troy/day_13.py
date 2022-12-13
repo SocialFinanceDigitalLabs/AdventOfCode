21fb459f7b6d3d38ca830e6d859786c3e6c5cc19
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2022-12-13 18:58:55 +0000

    Update day_13.py

--- solutions/2022/patrick-troy/day_13.py
+++ solutions/2022/patrick-troy/day_13.py
@@ -5,7 +5,7 @@ session = "53616c7465645f5f932620b9dd9cb53e9facee9282730977ff26a7c28126db6fa8ce7
 
 packet_pairs = get_data(day=13, year=2022, session=session).split()
 
-test = open(r"C:\Users\patrick.troy\Downloads\test.txt").read().split()
+test = open(r"C:\Users\Patrick\Downloads\test.txt").read().split()
 
 
 def group_list(data, group_size):
@@ -16,13 +16,57 @@ def group_list(data, group_size):
 grouped_pairs = group_list(test, 2)
 
 
+def check_bigger_int(left: int, right: int):
+    if left == right:
+        return "equal"
+    elif left < right:
+        return "right order"
+    elif left > right:
+        return "wrong order"
+
+
+def check_both_different(left, right):
+    if isinstance(left, list) and isinstance(right, int):
+        right = [right]
+        compare(left, right)
+    if isinstance(left, int) and isinstance(right, list):
+        left = [left]
+        compare(left, right)
+
+
+def compare(left, right):
+    order = None
+    if isinstance(left, list) and isinstance(right, list):
+        max_len = max(len(left), len(right))
+        for value in range(max_len):
+            try:
+                compare(left[value], right[value])
+            except IndexError:
+                if len(left) < len(right):
+                    order = "len, right order"
+                else:
+                    order = "len, wrong order"
+        return order
+    if isinstance(left, int) and isinstance(right, int):
+        if check_bigger_int(left, right) == "right order":
+            order = "right order"
+        if check_bigger_int(left, right) == "wrong order":
+            order = "wrong order"
+        else:
+            pass
+        return order
+    else:
+        return check_both_different(left, right)
+
+
+pair_dict = {}
+index = 1
 for group in grouped_pairs:
+    print(group)
     left, right = eval(group[0]), eval(group[1])
-    print(left, right)
+    pair_dict[index] = compare(left, right)
+    print(compare(left, right))
+    index += 1
 
 
-
-
-L = "[[], [1], [[[1, 3], 2, 1, 3]]]"
-R = "[[[], 6, [3, 8]], []]"
-
+print(pair_dict)
